657724385d2959fff865e159c25ceb6dbd5bd271
znm2500 <89569855+znm2500@users.noreply.github.com>
2025-11-11 14:57:28 +0800

    1

--- Shadertoy to Gamemaker/obj/Debug/net8.0-windows/Shadertoy to Gamemaker.AssemblyInfo.cs
+++ Shadertoy to Gamemaker/obj/Debug/net8.0-windows/Shadertoy to Gamemaker.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025 znm2500")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("用于便捷把shadertoy转化为yymps")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f1d0a7a21cee6a9d8d295f6aadc6022199eb39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06fc2a1e7b53b61acbdaf8055601a1fceeef601")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shadertoy_to_Gamemaker Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shadertoy to Gamemaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
